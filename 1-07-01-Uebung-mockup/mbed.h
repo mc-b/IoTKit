aa3802be9b4bfa5d0ec20fb568de0bf52d6284b9
mc-b <marcel.bernet@ch-open.ch>
2015-08-05 11:48:42 +0200

    Beispiele fuer lokale Compilation

---
+++ 1-07-01-Uebung-mockup/mbed.h
@@ -0,0 +1,21 @@
+/*
+ * Mock-up fuer mbed.h
+ */
+
+#ifndef MBED_H_
+#define MBED_H_
+
+#include <cstddef>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+
+#include <unistd.h>
+
+// Useful C libraries
+#include <math.h>
+#include <time.h>
+
+#define wait( x )	sleep( x )
+
+#endif /* MBED_H_ */
