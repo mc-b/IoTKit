7491be5516a0ae9132d5fd39b71005a1d2053497
mbern <mbern@iot>
2015-11-29 20:07:20 +0100

    Umgestellt auf restart=always

--- docker/apache-rest/start.sh
+++ docker/apache-rest/start.sh
@@ -4,18 +4,23 @@
 #
 service mysql start
 
-# Erstellen der Datenbank, User und Testdaten
-mysql -u root <<%EOF%
-create database if not exists sensoren;
-create user 'www-data'@'localhost' identified by 'mbed'; 
-grant usage on *.* to 'www-data'@'%' identified by 'mbed';
-grant all privileges on sensoren.* to 'www-data'@'%';
-flush privileges;
-use sensoren;
-create table data ( seq INT PRIMARY KEY AUTO_INCREMENT, poti FLOAT, light FLOAT, hall FLOAT, temp FLOAT, created TIMESTAMP DEFAULT CURRENT_TIMESTAMP );
-insert into data(poti, light, hall, temp) values ( 0.9, 0.8, 0.49, 25.2 );
-insert into data(poti, light, hall, temp) values ( 0.8, 0.7, 0.48, 25.1 );
+if [ ! -f /CREATED ]
+then
+	# Erstellen der Datenbank, User und Testdaten
+	mysql -u root <<%EOF%
+	create database if not exists sensoren;
+	create user 'www-data'@'localhost' identified by 'mbed'; 
+	grant usage on *.* to 'www-data'@'%' identified by 'mbed';
+	grant all privileges on sensoren.* to 'www-data'@'%';
+	flush privileges;
+	use sensoren;
+	create table data ( seq INT PRIMARY KEY AUTO_INCREMENT, poti FLOAT, light FLOAT, hall FLOAT, temp FLOAT, created TIMESTAMP DEFAULT CURRENT_TIMESTAMP );
+	insert into data(poti, light, hall, temp) values ( 0.9, 0.8, 0.49, 25.2 );
+	insert into data(poti, light, hall, temp) values ( 0.8, 0.7, 0.48, 25.1 );
 %EOF%
+	
+	touch /CREATED
+fi	
 
 # Starten Apache Daemon
 /bin/bash -c "source /etc/apache2/envvars && exec /usr/sbin/apache2 -DFOREGROUND"
--- docker/thingspeak/start.sh
+++ docker/thingspeak/start.sh
@@ -4,17 +4,24 @@
 #
 service mysql start
 
-# Erstellen des ThingSpeak User 
-mysql -u root <<%EOF%
-create user 'thing'@'localhost' identified by 'speak'; 
-grant usage on *.* to 'thing'@'localhost' identified by 'speak';
-grant all privileges on *.* to 'thing'@'localhost';
-flush privileges;
-%EOF%
+# Datenbank noch nicht initialisiert
+if [ ! -f /CREATED ] 
+then
 
-cd /opt/thingspeak
-rake db:create
-rake db:schema:load
+	# Erstellen des ThingSpeak User 
+	mysql -u root <<%EOF%
+	create user 'thing'@'localhost' identified by 'speak'; 
+	grant usage on *.* to 'thing'@'localhost' identified by 'speak';
+	grant all privileges on *.* to 'thing'@'localhost';
+	flush privileges;
+%EOF%
+	
+	cd /opt/thingspeak
+	rake db:create
+	rake db:schema:load
+	
+	touch /CREATED
+fi
 
 # Ruby on Rails Server mit ThingSpeak
 rails server
