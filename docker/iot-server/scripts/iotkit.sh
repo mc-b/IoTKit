cf5c7d531727f0e2bd317fc567f48d212dc59963
mc-b <marcel.bernet@ch-open.ch>
2016-01-04 15:03:38 +0100

    Sourcen und Container getrennt

---
+++ docker/iot-server/scripts/iotkit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# IoTKit Sources
+sudo -u vagrant mkdir ws
+cd ws
+sudo -u vagrant git clone https://github.com/mc-b/IoTKit.git
--- docker/iot-stick/scripts/container.sh
+++ docker/iot-stick/scripts/container.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# Sources
-sudo -u vagrant mkdir ws
-cd ws
-sudo -u vagrant git clone https://github.com/mc-b/IoTKit.git
-
 # install and start Apache & Docker Container  
-cd IoTKit/docker
+cd /home/vagrant/ws/IoTKit/docker
 make install
 make run
