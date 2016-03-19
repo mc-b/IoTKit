10107361b6e04a4ce1d2e0359b7f04e98745de6e
mc-b <marcel.bernet@ch-open.ch>
2016-03-19 11:27:24 +0100

    hostname iot-server, email (postfix), TZ, Keyboard

---
+++ docker/iot-server/scripts/email.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+# E-Mail Standard Installation
+
+sudo debconf-set-selections <<< "postfix postfix/mailname string iot-server"
+sudo debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet with Smarthost'"
+export DEBIAN_FRONTEND=noninteractive
+
+sudo apt-get install -y postfix bsd-mailx 
---
+++ docker/iot-server/scripts/osconfig.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Grundkonfiguration OS
+#
+
+# Change hostname	
+export NEW_HOSTNAME=iot-server
+export CURRENT_HOSTNAME=`cat /etc/hostname | tr -d " \t\n\r"`
+echo $NEW_HOSTNAME > /etc/hostname
+sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\t$NEW_HOSTNAME/g" /etc/hosts
+
+# Locale
+localedef -c -f UTF-8 -i de_CH de_CH.UTF-8
+update-locale LANG=de_CH.UTF-8
+
+# Keyboard
+export NOOBSKEYBOARD=ch
+sed -i /etc/default/keyboard -e "s/^XKBLAYOUT.*/XKBLAYOUT=\"$NOOBSKEYBOARD\"/"
+dpkg-reconfigure -f noninteractive keyboard-configuration
+invoke-rc.d keyboard-setup start
+
+# TimeZone
+echo "Europe/Zurich" > /etc/timezone 
+dpkg-reconfigure -f noninteractive tzdata
+
+# Hilfsprogramme
+sudo apt-get install -y autossh
+
+
